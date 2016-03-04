ab2c7fa091a392696f169b39f5cf26704b0004c9
Chai Phonbopit <phonbopit@gmail.com>
2016-03-04 12:01:20 +0700

    update index and api

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
 
 const Hapi = require('hapi');
-const Omise = require('omise');
-
-let omise = Omise({
-  secretKey: '',
-  omiseVersion: '2015-09-10'
-});
 
 const server = new Hapi.Server();
 
